b71433e04b6dcc9623070599e1919ab39ac98284
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-04-04 19:27:57 +0530

    make changes to convert json file into array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+// const URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
 
 const tableBody = document.getElementById("table").getElementsByTagName("tbody")[0];
@@ -8,47 +8,31 @@ const SortByPercentageBtn = document.getElementById("sortbypercentagebtn");
 const searchBtn = document.getElementById("searchbtn");
 const searchInput = document.getElementById('searchbar');
 
-async function fetchData(URL) {
+async function fetchData() { //if i have url then we need to pass url as parameter in fetchData
   try {
+
+    //converting json file into array format
     const response = await fetch('data.json'); 
-    const data = await response.json();
+    const data = await response.json(); 
     renderData(data);
-    // return data;
+    return data;
   } catch (error) {
     console.error("Error:", error);
   }
-}
-
-
-function renderData(data) {
-  tableBody.innerHTML = ""; // Clear existing data
-  for (let coin of data) {
-    const row = document.createElement("tr");
-    row.innerHTML = `
-
-        <td>${coin.name}</td>
-        <td>${coin.id}</td>
-        <td><img src="${coin.image}" id="image" alt="${coin.name}"></td>
-        <td>${coin.symbol}</td>
-        <td>$ ${coin.current_price}</td>
-        <td>${coin.total_volume}</td>
-      `;
-    tableBody.appendChild(row);
-  }
-}
-
+};
+// adding eventlistener for sorting by market_caps
 SortBymktCapBtn.addEventListener("click", function (e) {
     e.preventDefault();
     sortByMarketCap();
   });
 
-
+// adding eventlistener for sorting by percentageChange
   SortByPercentageBtn.addEventListener("click", function (e) {
     e.preventDefault();
     sortByChange();
   });
 
-
+// debounce
 document.addEventListener('DOMContentLoaded', function () {
     fetchData();
     const searchInput = document.getElementById('searchbar');
@@ -69,17 +53,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+
 // Function to sort data by market cap
-function sortByMarketCap() {
-  const sortedData = data.sort((a, b) => Number(b.market_cap) - Number(a.market_cap)); 
-  renderData(sortedData);
+async function sortByMarketCap() {
+
+  
+    try {
+      const response = await fetch('data.json'); 
+      const data = await response.json();
+      const sortedData = data.sort((a, b) => Number(b.market_cap) - Number(a.market_cap)); 
+      console.log(sortedData);
+      renderData(sortedData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  
 }
 
 // Function to sort data by percentage change
-function sortByChange() {
-  const sortedData = data.sort(
-    (a, b) => Number(b.price_change_percentage_24h) - Number(a.price_change_percentage_24h));
-  renderData(sortedData);
+async function sortByChange() {
+  try {
+    const response = await fetch('data.json'); 
+    const data = await response.json();
+    const sortedData = data.sort(
+      (a, b) => Number(b.price_change_percentage_24h) - Number(a.price_change_percentage_24h));
+      console.log(sortedData);
+    renderData(sortedData);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 
@@ -98,6 +100,24 @@ async function searchByName() {
         console.error('Error fetching data:', error);
     }
 }
+
+
+function renderData(data) {
+  tableBody.innerHTML = ""; // Clear existing data
+  for (let coin of data) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+
+        <td>${coin.name}</td>
+        <td>${coin.id}</td>
+        <td><img src="${coin.image}" id="image" alt="${coin.name}"></td>
+        <td>${coin.symbol}</td>
+        <td>$ ${coin.current_price}</td>
+        <td>${coin.total_volume}</td>
+      `;
+    tableBody.appendChild(row);
+  }
+} 
 
 
 
